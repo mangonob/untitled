ce36a5e494d19a9d9b074b66f5ab98bf2826a340
mangonob <534393806@qq.com>
2016-01-22 22:16:11 +0800

    commit

---
+++ src/com/company/Shell.java
@@ -0,0 +1,54 @@
+package com.company;
+
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Created by mangonob on 16/1/22.
+ */
+public class Shell {
+
+    public static boolean less(Comparable v, Comparable w) {
+
+        return v.compareTo(w) < 0;
+    }
+
+    public static void exch(Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    public static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            StdOut.print(a[i] + " ");
+        }
+        StdOut.println();
+    }
+
+    public static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++) {
+            if (less(a[i], a[i - 1])) return false;
+        }
+        return true;
+    }
+
+    public static void sort(Comparable[] a) {
+        int N = a.length;
+
+        int h = 1;
+        while ( h < N/3 ) h = 3*h + 1;
+        while (h >= 1) {
+            for (int i = h; i < N; i ++) {
+                Comparable temp = a[i];
+
+                int j = i;
+                for(; j >= h && less(temp, a[j - h]) ;j-=h) {
+                    a[j] = a[j - h];
+                }
+                a[j] = temp;
+            }
+
+            h = h/3;
+        }
+    }
+}
---
+++ src/com/company/SortCompare.java
@@ -0,0 +1,47 @@
+package com.company;
+
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.Stopwatch;
+
+/**
+ * Created by mangonob on 16/1/22.
+ */
+
+public class SortCompare {
+    public static double time (String alg, Comparable[] a) {
+        Stopwatch timer = new Stopwatch();
+
+        if(alg.equals("Insertion")) com.company.Insertion.sort(a);
+        if(alg.equals("Selection")) com.company.Selection.sort(a);
+        if(alg.equals("Shell")) com.company.Shell.sort(a);
+        if(alg.equals("InsertionWithSentry")) com.company.InsertionWithSentry.sort(a);
+
+        return timer.elapsedTime();
+    }
+
+    public static double timeRandomInput(String alg, int N, int T) {
+        double total = 0.0;
+
+        Double[] a = new Double[N];
+        for (int  t = 0; t < T; t ++) {
+            for (int i = 0; i < N; i++) {
+                a[i] = StdRandom.uniform();
+            }
+            total += time(alg, a);
+        }
+
+        return total;
+    }
+
+    public static void main(String[] args) {
+        String alg1 = args[0];
+        String alg2 = args[1];
+        int N = Integer.parseInt(args[2]);
+        int T = Integer.parseInt(args[3]);
+        double t1 = timeRandomInput(alg1, N, T);
+        double t2 = timeRandomInput(alg2, N, T);
+        StdOut.printf("For %d random Doubles %s is\n", N, alg1);
+        StdOut.printf(" %.1f times faster than %s\n", t2/t1, alg2);
+    }
+}
